3f364662e2b0987149ac64893bc5174378180a57
unknown <celi.ho@gmail.com>
2018-11-29 01:44:43 -0500

    initial site files

---
+++ isitfridayyet.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Is It Friday Yet?</title>
+    <style type="text/css">
+    body {
+    	margin: 0;
+    	padding: 0;
+      background: black;
+    }
+    #main-content {
+      margin: 30px;
+      text-align: center;
+      color: skyblue;
+    }
+    #main-content h1 {
+      font: 40px Arial, Helvetica, sans-serif;
+    }
+    #main-content p {
+      font: 24px "Times New Roman", Times, Georgia, serif;
+    }
+    #main-content p strong {
+      font-size: 70px;
+      color: white;
+    }
+  	</style>
+  </head>
+  <body>
+    <div id="main-content">
+      <h1>Is It Friday Yet?</h1>
+      
+      <p>
+        <?php date_default_timezone_set("America/New_York"); ?>
+
+        <?php
+        if(date('D') == 'Fri') {
+          echo "Woo Hoo! It is Friday!!!";
+        } else {
+          echo "Sorry, it is not Friday yet.<br />";
+          $remaining = 5 - date('N');
+          if($remaining < 0) {$remaining += 7;}
+          echo "Only {$remaining} more days to go.";
+        }
+          
+        ?>
+      </p>
+      
+    </div>
+    
+  </body>
+</html>
